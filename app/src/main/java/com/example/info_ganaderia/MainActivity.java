e8eab6eb25e840ac7aaa11c1d9eea283e5ec6c8c
pikeras72 <diegopikeras@gmail.com>
2023-02-18 21:44:17 +0100

    initial menu added

--- app/src/main/java/com/example/info_ganaderia/MainActivity.java
+++ app/src/main/java/com/example/info_ganaderia/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.info_ganaderia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -21,6 +22,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void irARegistrarse(View view){
         Intent i = new Intent(this, registrarActivity.class);
+        startActivity(i);
+    }
+
+    public void irANoticias(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://elpais.com/noticias/ganaderia/"));
+        startActivity(i);
+    }
+    public void irATipos(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://economipedia.com/definiciones/tipos-de-ganaderia.html"));
+        startActivity(i);
+    }
+    public void irARazas(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.mapa.gob.es/es/ganaderia/temas/zootecnia/razas-ganaderas/razas/catalogo-razas/"));
+        startActivity(i);
+    }
+    public void irAConsejos(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://milkmaxfrio.com.ec/5-tips-para-tener-una-ganaderia-exitosa/"));
         startActivity(i);
     }
 }
